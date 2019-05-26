348890f60b6ab351417cfb2de1c52602dc917faf
bwlu <18232386357@163.com>
2019-05-26 16:38:58 +0800

    child_page add send_headers

--- data_collect/poa_scrapy/baiduspiderProject_new/baiduspider/child_page.py
+++ data_collect/poa_scrapy/baiduspiderProject_new/baiduspider/child_page.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import requests
 from lxml import etree
-def child_page(child_url,method='get',is_headers=False):
+def child_page(child_url,method='get',is_headers=False,send_headers=None):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'}
+    headers = send_headers if send_headers!=None else headers
     if method=='get' and is_headers:
         response = requests.get(child_url, headers=headers)
     elif method=='get':
