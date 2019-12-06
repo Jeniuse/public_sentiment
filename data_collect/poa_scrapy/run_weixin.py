9e9b051cbc9c8a1d8b00158772463f0717b94f1a
bwlu <18232386357@163.com>
2019-12-06 14:01:41 +0800

    微信每天爬取两次

--- data_collect/poa_scrapy/run_weixin.py
+++ data_collect/poa_scrapy/run_weixin.py
@@ -12,17 +12,13 @@ def main():
     fp = open('out_put', 'w')#用来输出错误信息
     stderr = sys.stderr
     sys.stderr = fp
-
-    while True:
-       t = threading.Thread(target=weixinspider)
-	
-       t.start()
-       t.join()
-       print(" weixin 执行完成一轮")
-       time.sleep(300)
+    
+    t = threading.Thread(target=weixinspider)
+    t.start()
+    t.join()
+    print(" weixin 执行完成一轮")
 
     fp.close()
     sys.stderr = stderr
-		
-	
+
 main()
